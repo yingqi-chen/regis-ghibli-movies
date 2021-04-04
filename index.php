5de3fa8eedd87fbe03612b9c27048da231929170
yingqi-chen <chanwk18@hotmail.com>
2021-04-04 06:41:29 -0600

    Finish Read

--- index.php
+++ index.php
@@ -9,14 +9,38 @@
      die("Failed to grab result.");
  }
 
+
 $rows = $result->num_rows;
 
 for($j = 0; $j < $rows; ++$j){
     $row = $result->fetch_array(MYSQLI_ASSOC);
-    echo "<strong>Director</strong>: <br>", htmlspecialchars($row["director_id"]), "<br>";
+    $director_id = $row["director_id"];
+    $director = grab_director_info($conn, $director_id);
+
+    if ($director){
+        $director_name = $director["director_name"];
+    }else{
+        $director_name = "unknown";
+    }
+
+    echo "<strong>Director</strong>: <br>", $director_name, "<br>";
     echo "<strong>Title</strong>: <br>", htmlspecialchars($row["title"]), "<br>";
     echo "<strong>Year</strong>: <br>", htmlspecialchars($row["year"]), "<br>";
     echo "<br>";
+}
+
+function grab_director_info($conn, $director_id){
+    $grab_director_query = "SELECT * FROM directors WHERE id = $director_id ";
+    $result = $conn->query($grab_director_query);
+    if (!$result){
+        return null;
+    }else{
+        $row = $result->fetch_array(MYSQLI_ASSOC);
+        return array(
+            "director_name" => htmlspecialchars($row["name"]),
+            "director_introduction" => htmlspecialchars($row["introduction"])
+        );
+    }
 }
 
 $result->close();

4512b9c8970208e020ad25b04c9f71c34717367f
yingqi-chen <chanwk18@hotmail.com>
2021-04-11 15:11:09 -0600

    Add signup page

---
+++ signup.php
@@ -0,0 +1,46 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <style type="text/css">
+        .wrapper{
+            margin: 6% auto ;
+        }
+        .form-wrapper{
+            width: 50%;
+            margin: 0 auto;
+            padding-left: 15%;
+        }
+        .button-middle{
+            width: 50%;
+            padding-left: 18%;
+        }
+    </style>
+    <sript>
+</head>
+<body>
+<div class="wrapper w-50">
+    <div class="container">
+        <h2 class="text-center my-5">Sign up</h2>
+        <p class="text-center h5 my-5">So you can start to create, update, delete movies for the website!</p>
+        <div class="form-wrapper">
+            <form action="signup.php" method="post" onsubmit="return validate(this)">
+                <label for="username" class="form-label">Username</label><br>
+                <input type="text" name="username"><br>
+                <label for="password" class="form-label">Password</label> <br>
+                <input type="text" name="password"><br>
+                <label for="email" class="form-label">Email</label> <br>
+                <input type="text" name="email"><br>
+                <br>
+                <div class="button-middle"><input type="submit" value="Sign up" class="btn btn-outline-secondary btn-sm" ></div>
+            </form>
+        </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>

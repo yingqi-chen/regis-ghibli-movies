0b3d3fa03c7605556a8249626dfd0265bf1491cd
yingqi-chen <chanwk18@hotmail.com>
2021-04-18 15:56:28 -0600

    Change random theme color

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-$username = $_SESSION['username']
+$username = $_SESSION['username'];
+
 ?>
 
 <!DOCTYPE html>
@@ -11,10 +12,20 @@ $username = $_SESSION['username']
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <style type="text/css">
     </style>
+    <script>
+        function randomIndex(){
+            return Math.floor(Math.random() * 6);
+        }
+        function changeTheme(){
+            let colors = ["#FADBD8", "#e3f2fd", "#D5F5E3", "#E8DAEF", "#FCF3CF", "#EDBB99"];
+            let color = colors[randomIndex()];
+            document.getElementsByTagName("nav")[0].style.backgroundColor = color;
+        }
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #D5F5E3 ">
     <div class="container-fluid">
         <a class="navbar-brand" href="index.php">Ghibli Wiki</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -36,9 +47,14 @@ _USERLOGGEDIN;
 _USERNOTLOGGEDIN;
                 }
             ?>
+            <button class="btn btn-outline-primary" onclick="changeTheme()">Change a random color!</button>
             </div>
         </div>
     </div>
 </nav>
 </body>
 </html>
+
+
+
+

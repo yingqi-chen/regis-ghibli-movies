25830c8563309468bdc230c1e12344bec1b9302f
yingqi-chen <chanwk18@hotmail.com>
2021-04-25 06:45:06 -0600

    Refactor delete.php

--- delete.php
+++ delete.php
@@ -1,37 +1,4 @@
 <?php
-//require_once 'db.php';
-//require_once 'functions.php';
-//include_once "header.php";
-//
-//session_start();
-//$username = $_SESSION['username'];
-//if (!$username){
-//    header("location: index.php?error=needauthentication");
-//    return ("You are not allowed to do this operation");
-//}
-//
-//
-//if (isset($_POST['delete'])) {
-//    $id = $_POST['id'];
-//
-//    if (!empty($id)) {
-//      return ("The id I got is: $id");
-//    }else{
-//      return ("Try again, the id of the movie is not valid.");
-//    }
-//}else{
-//    $param_id = mysql_entities_fix_string($conn, $_GET["id"]);
-//    $movie = query_movie($conn, $param_id);
-//    $name = $movie['title'];
-//}
-//
-//return json_encode(array("message" => "testing"));
-//
-//$conn->close();
-//?>
-
-
-<?php
 // Headers that allows being queried from everywhere, receiving json, post method
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
@@ -46,7 +13,6 @@ $data = json_decode(file_get_contents("php://input"));
 $id = $data->id;
 
 // Delete post
-
 if (!empty($id)) {
     $result = delete_movie($conn, 15);
     if($result){
@@ -55,33 +21,17 @@ if (!empty($id)) {
                 array("message"=>"Deleted Successfully!")
             );
         };
-        echo json_encode(array("error_message" =>"Movie doesn't exist \n", "error"=>"Delete failed"));
+        echo json_encode(array("error"=>"Delete failed", "error_message" =>"Movie doesn't exist" ));
     } else{
-        echo json_encode(array("error_message" =>"errorsssss \n", "error"=>"Delete failed"));
+        echo json_encode(array("error"=>"Delete failed","error_message" =>"Failed due to unexpected error."));
+        header('HTTP/1.1 500 Internal Server Error');
     }}else{
-    echo json_encode(array("error_message" =>"2 errorsssss \n", "error"=>"Delete failed"));
+    echo json_encode(array("error"=>"Delete failed","error_message" =>"ID is not valid"));
 
 }
 
-//    if($result){
-//        echo json_encode(
-//            array("message"=>"Deleted Successfully!")
-//        );
-//    }else{
-//        echo json_encode(
-//            array("error"=>"Delete failed")
-//        );
-//    }
-//}else{
-//    echo json_encode(array("error"=>"id is not valid."));
-//}
 
 
-//else{
-//    $param_id = mysql_entities_fix_string($conn, $_GET["id"]);
-//    $movie = query_movie($conn, $param_id);
-//    $name = $movie['title'];
-//}
 
 
 

b7b96d405b657322d3e426f4fb0eac1080913c68
yingqi-chen <chanwk18@hotmail.com>
2021-05-02 16:49:29 -0600

    Add dotenv to secure my important credential

--- config/db.php
+++ config/db.php
@@ -1,9 +1,14 @@
 <?php
 
+  require_once "vendor/autoload.php";
+
+  $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ ."/../");
+  $dotenv->load();
+
   $host = 'localhost';
   $database = 'ghibli_movies';
-  $user = 'regis';
-  $pass = 'MSSEregis2021~';
+  $user = $_ENV["DB_USER"];
+  $pass = $_ENV["DB_PASSWORD"];
 
   $conn = new mysqli($host, $user, $pass, $database);
 
